20251cad9eb04867f3c592b5a81c530665760fcf
Mateus Maccieri <mateusmaccieri@dhcp-210-144.cc.gettysburg.edu>
2018-12-09 15:14:19 -0500

    cleaned and commented files

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,7 +5,6 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
 import SettingsNavigator from '../navigation/SettingsNavigator';
 
 const HomeStack = createStackNavigator({
